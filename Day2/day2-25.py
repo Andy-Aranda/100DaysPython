9696829ec4cdeb2659d426796807d8daeb7b0255
Andrea Aranda <andreaar1605@gmail.com>
2023-03-01 18:34:58 -0600

    Ejercicio clase 25 modulo 2, dias, semanas y meses que me quedan

---
+++ Day2/day2-25.py
@@ -0,0 +1,28 @@
+'''I was reading this article by Tim Urban - Your Life in Weeks and realised just how little time we actually have.
+
+https://waitbutwhy.com/2014/05/life-weeks.html
+
+Create a program using maths and f-Strings that tells us how many days, weeks, months we have left if we live until 90 years old.
+
+It will take your current age as the input and output a message with our time left in this format:
+
+You have x days, y weeks, and z months left.
+
+Where x, y and z are replaced with the actual calculated numbers.
+
+Hint
+There are 365 days in a year, 52 weeks in a year and 12 months in a year.
+Try copying the example output into your code and replacing the relevant parts so that the sentence is formated the same way.'''
+
+# 🚨 Don't change the code below 👇
+age = input("What is your current age? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+years = 90 - (int(age))
+months = years * 12
+weeks = years * 52
+days = years * 365
+
+print(f"You have {days} days, {weeks} weeks, and {months} months left")
