ed10448a51e7010996bfbe6c9d49cd9df7493f51
Andrea Aranda <andreaar1605@gmail.com>
2023-03-07 21:43:10 -0600

    Se agregaron ejercicios de este módulo

---
+++ Day6/day61.py
@@ -0,0 +1,11 @@
+'''Para hacer mi propia función'''
+
+def my_function():
+    print("Hello")
+    print("Bye")
+
+
+'''No va a hacer nada porque no la he llamado'''
+
+my_function()
+
---
+++ Day6/while_loops.py
@@ -0,0 +1,39 @@
+i = 0
+while i < 3:
+    print("Hola")
+    i += 1
+
+print("\n")
+
+j = 3
+while j > 0:
+    print("Hola")
+    j-= 1
+
+
+#i = 5
+'''
+Ver ejercicio en https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%202&url=worlds%2Ftutorial_en%2Fhurdle2.json
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump(turn_right):
+        move()
+        turn_left()
+        move()
+        turn_right()
+        move()
+        turn_right()
+        move()
+        turn_left()
+
+   
+while at_goal() == False:
+    jump(turn_right)
+    #i -= 1
+
+
+'''
