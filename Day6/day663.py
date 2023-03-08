f80eb8391acddeae7f753a91b181ee8b966755ca
Andrea Aranda <andreaar1605@gmail.com>
2023-03-07 23:00:06 -0600

    CNuevos ejercicios

---
+++ Day6/day663.py
@@ -0,0 +1,42 @@
+'''
+https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%203&url=worlds%2Ftutorial_en%2Fhurdle3.json
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def jump():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    
+while at_goal() == False:
+    while front_is_clear() == False:
+        jump()
+    while front_is_clear() == True and at_goal() == False:
+        move()
+
+    def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def jump():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    
+while not at_goal():
+   if wall_in_front():
+        jump()
+   else:
+        move()
+        '''
