4d001bce7ebbc5f09952f7ebac186bd1c9dce062
Andrea Aranda <andreaar1605@gmail.com>
2023-03-13 19:27:14 -0600

    Nuevo ejercicio prime numbers

---
+++ Day8/81.py
@@ -0,0 +1,26 @@
+'''# Review: 
+# Create a function called greet(). 
+def greet():
+    # Write 3 print statements inside the function.
+    print("Hello")
+    print("I am Andrea")
+    print(":)")
+
+# Call the greet() function and run your code.
+
+greet()
+
+
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"How do you do {name}?")
+
+greet_with_name("Candy")
+
+'''
+
+def greet_with (name, location):
+    print(f"Hello {name}")
+    print(f"What is like in {location}?")
+
+greet_with(location = "Lyon", name = "Andrea")
