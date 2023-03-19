56d2c7a7df3b59327d287aba4ee0b6da978e9214
Andrea Aranda <andreaar1605@gmail.com>
2023-03-18 23:54:06 -0600

    Auction program finished

---
+++ Day9/auction_program.py
@@ -0,0 +1,37 @@
+from art import logo
+import os
+
+print(logo)
+
+
+
+def apuestas(nombre, valor):
+
+    apuestas = {}
+    apuestas[nombre] = valor
+    max_valor = 0
+    max_apostador = 0
+    max_apostador = "Nadie"
+
+    respuesta = input("Quieres agregar a alguien? y/n: ").lower()
+
+
+    while respuesta == "y":
+        os.system("clear")
+        nombre = input("Whats your name? ")
+        valor = int(input("Whats your bid? "))
+        if valor > max_valor:
+            max_valor = valor
+            max_apostador = nombre
+            apuestas[nombre] = valor
+
+        respuesta = input("Quieres agregar a alguien? y/n: ").lower()
+
+
+    print(f"El ganador es {max_apostador} con {max_valor}")
+
+
+nombre = input("Whats your name? ")
+valor = int(input("Whats your bid? "))
+
+apuestas(nombre, valor)
