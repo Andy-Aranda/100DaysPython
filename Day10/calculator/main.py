06b11b554e5853989b16f4b99f8a601fb8cb3e6a
Andrea Aranda <andreaar1605@gmail.com>
2023-04-02 12:38:03 -0600

    Función que elige la operación a realizar, ya funciona

--- Day10/calculator/main.py
+++ Day10/calculator/main.py
@@ -1,24 +1,34 @@
 from art import logo
+print(logo)
 
 def add(a, b):
     '''Función para sumar dos números'''
     return a + b
-    pass
 
 def substract(a, b):
     '''Función para restar dos números'''
     return a - b
-    pass
 
 def division(a, b):
     '''Función para dividir dos números'''
     return a / b
-    pass
 
 def multiply(a, b):
     '''Función para multiplicar dos números'''
     return a * b
-    pass
+
+def operation(symbol, num1, num2):
+    if symbol == "+":
+        return num1 + num2
+    
+    elif symbol == "-":
+        return num1 - num2
+    
+    elif symbol == "/":
+        return num1 / num2
+    
+    else:
+        return num1 * num2
 
 operations = {
     #llama a las funciones de acuerdo a la clave que se elige
@@ -30,3 +40,13 @@ operations = {
 
 a = int(input("Enter the first number: "))
 b = int(input("Enter the second number: "))
+
+for symbol in operations:
+    print(symbol)
+
+operation_symbol = input("Choose an operation symbol from above: ")
+result = operation(operation_symbol, a, b) #guarda lo que retorne final_operation
+
+print(f"{a} {operation_symbol} {b} = {result}")
+
+
